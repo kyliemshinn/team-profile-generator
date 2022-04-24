4b1b2159d23285ef61eb574a8cd5004cc0a7efcc
kylie <kyliemshinn@gmail.com>
2022-04-24 15:01:07 -0700

    add section questions to index.js file

--- index.js
+++ index.js
@@ -1,1 +1,90 @@
 const inquirer = require('inquirer');
+
+
+const employeeSection = [
+    {
+        type: 'input',
+        message: "What is the employees name? ",
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: "What is the employees employee ID? ",
+        name: 'ID',
+    },
+    {
+        type: 'input',
+        message: "What is the employees email? ",
+        name: 'email',
+    },
+]
+
+const managerSection = [
+
+    {
+        type: 'input',
+        message: "What is the managers name? ",
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: "What is the managers employee ID? ",
+        name: 'ID',
+    },
+    {
+        type: 'input',
+        message: "What is the managers email? ",
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: "What is the managers office number? ",
+        name: 'number',
+    }
+]
+
+const engineerSection = [
+    {
+        type: 'input',
+        message: "What is the engineer name? ",
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: "What is the engineer employee ID? ",
+        name: 'ID',
+    },
+    {
+        type: 'input',
+        message: "What is the engineer email? ",
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: "What is the engineer github? ",
+        name: 'github',
+    }
+]
+
+const internSection = [
+    {
+        type: 'input',
+        message: "What is the intern name? ",
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: "What is the intern employee ID? ",
+        name: 'ID',
+    },
+    {
+        type: 'input',
+        message: "What is the intern email? ",
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: "What school does the intern attend? ",
+        name: 'school',
+    }
+]
